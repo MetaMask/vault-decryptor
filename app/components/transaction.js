76f9c8b8bb31a639b8cd39a045721a77558c13cd
Dan Finlay <dan@danfinlay.com>
2017-10-04 12:22:34 -0700

    Add better gas data

--- app/components/transaction.js
+++ app/components/transaction.js
@@ -1,6 +1,9 @@
 const Component = require('react').Component
 const h = require('react-hyperscript')
 const inherits = require('util').inherits
+const ethUtil = require('ethereumjs-util')
+const BN = ethUtil.BN
+const GWEI_FACTOR = new BN('1000000000', 10)
 
 module.exports = NewComponent
 
@@ -15,10 +18,16 @@ NewComponent.prototype.render = function () {
   const { id, time, status,
    metamaskNetworkId, txParams,
     gasLimitSpecified, estimatedGas,
-    history, hash, retryCount } = transaction
+    history, hash, retryCount, err } = transaction
 
+  // Date stuff
   const date = new Date(time)
   const dateString = date.toGMTString()
+
+  const { gasPrice } = txParams
+  const gasPriceHex = ethUtil.stripHexPrefix(gasPrice)
+  const gasPriceBN = new BN(gasPriceHex, 16)
+  const gasPriceString = gasPriceBN.div(GWEI_FACTOR).toString(10)
 
   let statusColor = 'white'
   switch (status) {
@@ -49,7 +58,11 @@ NewComponent.prototype.render = function () {
       h('p', `Time: ${dateString}`),
       h('p', `From: ${txParams.from}`),
       h('p', `To: ${txParams.to}`),
+      h('p', `Nonce: ${parseInt(txParams.nonce)}`),
+      h('p', `Gas Price: ${gasPriceString}`),
       h('p', `Status: ${status}`),
+      status === 'failed' ?
+        h('p', `Reason: ${JSON.stringify(err.message)}`) : null,
       h('p', `Hash: ${hash}`),
     ])
   )
